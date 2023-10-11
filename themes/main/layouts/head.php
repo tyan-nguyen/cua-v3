7d1121acbfea48c69d73599143c9fb3182f76046
tyan-nguyen <nguyenvantyan@gmail.com>
2023-10-11 10:10:09 +0700

    update favicon

--- themes/main/layouts/head.php
+++ themes/main/layouts/head.php
@@ -9,6 +9,12 @@
     <meta name="author" content="">
     <meta name="keywords" content="">
     <meta name="description" content="">
+    
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+
     <link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="/css/style.css?v=15">
     <link rel="stylesheet" type="text/css" href="/css/custom.css?v=19">
